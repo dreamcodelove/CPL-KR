e752135cb1dae5a85b3c6946551b08b687478276
Ashish Rai <rai.coldcharm@gmail.com>
2022-11-23 18:45:03 +0530

    Exercise 1-1

---
+++ Chapter-1/221123_1x1_experimenthelloworld.c
@@ -0,0 +1,17 @@
+/*
+ * FILENAME: 221123_1x1_experimenthelloworld.c
+ * PROGRAM: Experiment with Hello World
+ * AUTHOR: Ashish Rai
+ * DATE CREATED: 23/11/2022
+ * LAST MODIFIED: 23/11/2022
+ *
+ * DESCRIPTION: Run the "hello, world" program on your system. Experiment with
+ * leaving out parts of the program, to see what error messages you get.
+ */
+
+#include <stdio.h>
+int main() {
+    // print "hello, world" on console
+
+    printf("hello, world\n");
+}
