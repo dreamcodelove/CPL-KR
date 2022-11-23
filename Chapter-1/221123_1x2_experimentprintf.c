dd29be99e4be4c3dd29abac645da78fd3724e9f8
Ashish Rai <rai.coldcharm@gmail.com>
2022-11-23 18:46:03 +0530

    Exercise 1-2

---
+++ Chapter-1/221123_1x2_experimentprintf.c
@@ -0,0 +1,17 @@
+/*
+ * FILENAME: 221123_1x2_experimentprintf.c
+ * PROGRAM: Experiment with printf Function
+ * AUTHOR: Ashish Rai
+ * DATE CREATED: 23/11/2022
+ * LAST MODIFIED: 23/11/2022
+ *
+ * DESCRIPTION: Experiment to find out what happend when printf's argument
+ * string contains \c, where c is some character not listed above.
+ */
+
+#include <stdio.h>
+int main() {
+    // experiment with printf
+
+    printf("hello, \t\nworld\n");
+}
