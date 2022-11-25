9442e2e50b7b77703e1c43581b6490fdf32f0ff3
Ashish Rai <rai.coldcharm@gmail.com>
2022-11-25 22:05:12 +0530

    Exercise 1-7

---
+++ Chapter-1/221125_1x7_valueofeof.c
@@ -0,0 +1,15 @@
+/*
+ * FILENAME: 221125_1x7_valueofeof.c
+ * PROGRAM: Value of EOF
+ * AUTHOR: Ashish Rai
+ * DATE CREATED: 25/11/2022
+ * DATE MODIFIED: 25/11/2022
+ *
+ * DESCRIPTION: Exercise 1-7 Write a program to print the value of EOF.
+ */
+
+#include <stdio.h>
+
+int main() {
+    printf("Value of EOF: %d\n", EOF);
+}
